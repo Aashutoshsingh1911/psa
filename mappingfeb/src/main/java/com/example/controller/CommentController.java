9e32bd9296b3deffeeb259462bf7ae68d2debd4c
Aashutosh singh <Aashutoshsingh1911@gmail.com>
2024-07-22 18:05:22 +0530

    test1

---
+++ mappingfeb/src/main/java/com/example/controller/CommentController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+
+import com.example.entity.Comment;
+import com.example.entity.Post;
+import com.example.repository.CommentRepository;
+import com.example.repository.PostRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+
+public class CommentController {
+    private CommentRepository commentRepository;
+
+  private PostRepository postRepository;
+
+
+    public CommentController(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+    @PostMapping
+    public ResponseEntity<Comment>createcomment(
+
+           @RequestBody  Comment comment,
+           @RequestParam long postid
+    ){
+
+
+        Long postId;
+        Post post = postRepository.findById(postid).get();
+        comment.setPost(post);
+        return ResponseEntity.ok (commentRepository.save(comment));
+
+    }
+
+}
---
+++ mappingfeb/src/main/java/com/example/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.BusStop;
+import com.example.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    interface BusStopRepository extends JpaRepository<BusStop, Long> {
+    }
+}
